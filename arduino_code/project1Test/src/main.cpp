29c1fe97d780f382f789be931f0da32bbe36b1ba
KingCadia <57073780+KingCadia@users.noreply.github.com>
2023-09-11 12:10:46 -0400

    project1 matlab code

---
+++ arduino_code/project1Test/src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int sensorValue = analogRead(A0);
+  float Voltage = sensorValue * (5.0 / 1023.0);
+
+  Serial.println(Voltage);
+}
