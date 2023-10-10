9f70708f0e729d1f2b2ac3a80da16312206e8ce5
KingCadia <57073780+KingCadia@users.noreply.github.com>
2023-10-10 18:40:59 -0400

    got most stuff done

--- matlab_code/project_2/impross.m
+++ matlab_code/project_2/impross.m
@@ -6,73 +6,79 @@ classdef impross
         background
         forground
         well_locs
+        gamestate
+        height
+        width
+        subtraction
+        subtraction2
+        bw
     end
     
     methods
         function obj = impross(background, foreground)
-            [height, width, depth] = size(foreground);
-            subtraction = 255 - (background - foreground);
-            subtraction2 = imsubtraction(subtraction, height, width);
-            bw = imgetbw(subtraction2);
-            % gets the centroid locs
-            stats = regionprops(bw, 'Centroid');
-            centroids = cat(1, stats.Centroid);
-            well_locs = well_loc_find(background);
-                        
+            obj.background = background;
+            obj.forground = foreground;
+            [obj.height, obj.width, depth] = size(foreground);
+            sub = 255 - (background - foreground);
+            obj.subtraction = sub;
+            sub2 = obj.imsubtraction();
+            obj.subtraction2 = sub2;
+            bw = obj.imgetbw();
+            obj.bw = bw;
         end
        
-        function subtraction2 = imsubtraction(subtraction, height, width)
+        function sub2 = imsubtraction(obj)
             %does the threshholding for the colors to brighten 
-            subtraction2 = subtraction;
-            for i = 1:height
-                for j = 1:width
+            sub2 = obj.subtraction;
+            for i = 1:obj.height
+                for j = 1:obj.width
                     % check for red
-                    if (subtraction(i,j,1) <= 255 && subtraction(i,j,1) >= 200) && ...
-                       (subtraction(i,j,2) <= 225) && ...
-                       (subtraction(i,j,3) <= 140)
+                    if (obj.subtraction(i,j,1) <= 255 && obj.subtraction(i,j,1) >= 200) && ...
+                       (obj.subtraction(i,j,2) <= 225) && ...
+                       (obj.subtraction(i,j,3) <= 140)
                             % detected a red
-                            subtraction2(i,j,1) = 255;
-                            subtraction2(i,j,2) = 0;
-                            subtraction2(i,j,3) = 0;
+                            sub2(i,j,1) = 255;
+                            sub2(i,j,2) = 0;
+                            sub2(i,j,3) = 0;
                     end
 
                     % checks for green
-                    if ((subtraction(i, j, 1) >= 40) && (subtraction(i, j, 1) <= 140) ...
-                       && (subtraction(i, j, 2) >= 100) && (subtraction(i, j, 2) <= 220) ...
-                       && (subtraction(i, j, 3) >= 50) && (subtraction(i, j, 3) <= 150)) 
+                    if ((obj.subtraction(i, j, 1) >= 40) && (obj.subtraction(i, j, 1) <= 140) ...
+                       && (obj.subtraction(i, j, 2) >= 100) && (obj.subtraction(i, j, 2) <= 220) ...
+                       && (obj.subtraction(i, j, 3) >= 50) && (obj.subtraction(i, j, 3) <= 150)) 
                             % detected a green
-                            subtraction2(i,j,1) = 0;
-                            subtraction2(i,j,2) = 255;
-                            subtraction2(i,j,3) = 0;
+                            sub2(i,j,1) = 0;
+                            sub2(i,j,2) = 255;
+                            sub2(i,j,3) = 0;
                     end
 
                     % checks for blue
-                    if ((subtraction(i,j,1) <= 140 && subtraction(i,j,1) >= 100) && ...
-                       (subtraction(i,j,2) <= 160) && ...
-                       (subtraction(i,j,3) <= 220))
+                    if ((obj.subtraction(i,j,1) <= 140 && obj.subtraction(i,j,1) >= 100) && ...
+                       (obj.subtraction(i,j,2) <= 160) && ...
+                       (obj.subtraction(i,j,3) <= 220))
                             % detected a blue
-                            subtraction2(i,j,1) = 0;
-                            subtraction2(i,j,2) = 0;
-                            subtraction2(i,j,3) = 255;
+                            sub2(i,j,1) = 0;
+                            sub2(i,j,2) = 0;
+                            sub2(i,j,3) = 255;
                     end
 
                     % checks for yellow
-                    if ((subtraction(i,j,1) <= 255 && subtraction(i,j,1) >= 220) && ...
-                       (subtraction(i,j,2) <= 255 && subtraction(i,j,2) >= 240) && ...
-                       (subtraction(i,j,3) <= 200 && subtraction(i,j,3) >= 140))
+                    if ((obj.subtraction(i,j,1) <= 255 && obj.subtraction(i,j,1) >= 220) && ...
+                       (obj.subtraction(i,j,2) <= 255 && obj.subtraction(i,j,2) >= 240) && ...
+                       (obj.subtraction(i,j,3) <= 200 && obj.subtraction(i,j,3) >= 140))
                             % detected a yellow
-                            subtraction2(i,j,1) = 200;
-                            subtraction2(i,j,2) = 200;
-                            subtraction2(i,j,3) = 50;
+                            sub2(i,j,1) = 200;
+                            sub2(i,j,2) = 200;
+                            sub2(i,j,3) = 50;
                     end
                 end
             end
         end
         
         % gets a clean black and white image with noise filtered out 
-        function bw = imgetbw(sub)
+        function bw = imgetbw(obj)
             
-            bw = im2bw(sub, .8);
+            bw = im2bw(obj.subtraction2, .8);
             bw = ~bw;
             SE = strel('disk', 2);
             bw = imerode(bw, SE);
@@ -80,31 +86,9 @@ classdef impross
             SE = strel('disk', 6);
             bw = imdilate(bw, SE);
         end
-        
-        function well_loc_array = well_loc_find(background)
-            [height, width, depth] = size(background);
-            % gets all the well loc images
-            im1 = imread('well_loc_1.jpg');
-            im2 = imread('well_loc_2.jpg');
-            im3 = imread('well_loc_3.jpg');
-            im4 = imread('well_loc_4.jpg');
-            im5 = imread('well_loc_5.jpg');
-            im6 = imread('well_loc_6.jpg');
-            im7 = imread('well_loc_7.jpg');
-            im8 = imread('well_loc_8.jpg');
-            
-            well_loc_array = cat(im1, im2, im3, im4, ... 
-            im5, im6, im7, im8);
-            
-            % gets a clean black/white images for all well locs
-            for i = 1:size(well_loc_array)
-                im = well_loc_array(i);
-                im = 255 - (background - im);
-                im = imsubtraction(im, height, width);
-                im = imgetbw(im);
-                well_loc_array(i) = im;
-            end
-        end
+       
+       
     end
+
 end
 
--- matlab_code/project_2/main.m
+++ matlab_code/project_2/main.m
@@ -1,76 +1,102 @@
-clc;
 clear all;
-close all;
-%% gets the images and sizes
+clc
+% makes the obj that holds the compareing images
 background = imread('test_back.jpg');
-foreground = imread('yellow.jpg');
-[height, width, depth] = size(foreground);
-subtraction = 255 - (background - foreground);
-subtraction2 = subtraction;
-%does the threshholding for the colors to brighten 
-for i = 1:height
-    for j = 1:width
-        % check for red
-        if (subtraction(i,j,1) <= 255 && subtraction(i,j,1) >= 200) && ...
-           (subtraction(i,j,2) <= 225) && ...
-           (subtraction(i,j,3) <= 140)
-                % detected a red
-                subtraction2(i,j,1) = 255;
-                subtraction2(i,j,2) = 0;
-                subtraction2(i,j,3) = 0;
-        end
-        
-        % checks for green
-        if ((subtraction(i, j, 1) >= 40) && (subtraction(i, j, 1) <= 140) ...
-           && (subtraction(i, j, 2) >= 100) && (subtraction(i, j, 2) <= 220) ...
-           && (subtraction(i, j, 3) >= 50) && (subtraction(i, j, 3) <= 150)) 
-                % detected a green
-                subtraction2(i,j,1) = 0;
-                subtraction2(i,j,2) = 255;
-                subtraction2(i,j,3) = 0;
-        end
-        
-        % checks for blue
-        if ((subtraction(i,j,1) <= 140 && subtraction(i,j,1) >= 100) && ...
-           (subtraction(i,j,2) <= 160) && ...
-           (subtraction(i,j,3) <= 220))
-                % detected a blue
-                subtraction2(i,j,1) = 0;
-                subtraction2(i,j,2) = 0;
-                subtraction2(i,j,3) = 255;
-        end
-        
-        % checks for yellow
-        if ((subtraction(i,j,1) <= 255 && subtraction(i,j,1) >= 220) && ...
-           (subtraction(i,j,2) <= 255 && subtraction(i,j,2) >= 240) && ...
-           (subtraction(i,j,3) <= 200 && subtraction(i,j,3) >= 140))
-                % detected a yellow
-                subtraction2(i,j,1) = 200;
-                subtraction2(i,j,2) = 200;
-                subtraction2(i,j,3) = 50;
+foreground = imread('red.jpg');
+im = impross(background, foreground);
+
+% makes the gamestate struct
+gamestate.original = background;
+gamestate.well_color = [0, 0, 0, 0, 0, 0, 0, 0];
+gamestate.curret = foreground;
+gamestate.diffrence = im.subtraction;
+gamestate.noise_removal = im.bw;
+gamestate.pixel_locs = {[0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0]};
+% add in the key
+
+% gets the centoid locations
+stats = regionprops(im.bw, 'Centroid');
+num = size(stats);
+num = num(1);
+
+% gets the well loc bw image
+background = imread("well_loc_blank.jpg");
+foreground = imread("well_loc_1.jpg");
+im_holder = impross(background, foreground);
+well_loc_1 = im_holder.bw;
+
+background = imread("well_loc_blank.jpg");
+foreground = imread("well_loc_2.jpg");
+im_holder = impross(background, foreground);
+well_loc_2 = im_holder.bw;
+
+background = imread("well_loc_blank.jpg");
+foreground = imread("well_loc_3.jpg");
+im_holder = impross(background, foreground);
+well_loc_3 = im_holder.bw;
+
+background = imread("well_loc_blank.jpg");
+foreground = imread("well_loc_4.jpg");
+im_holder = impross(background, foreground);
+well_loc_4 = im_holder.bw;
+
+background = imread("well_loc_blank.jpg");
+foreground = imread("well_loc_5.jpg");
+im_holder = impross(background, foreground);
+well_loc_5 = im_holder.bw;
+
+background = imread("well_loc_blank.jpg");
+foreground = imread("well_loc_6.jpg");
+im_holder = impross(background, foreground);
+well_loc_6 = im_holder.bw;
+
+background = imread("well_loc_blank.jpg");
+foreground = imread("well_loc_7.jpg");
+im_holder = impross(background, foreground);
+well_loc_7 = im_holder.bw;
+
+background = imread("well_loc_blank.jpg");
+foreground = imread("well_loc_8.jpg");
+im_holder = impross(background, foreground);
+well_loc_8 = im_holder.bw;
+
+well_locs = {well_loc_1, well_loc_2, well_loc_3, well_loc_4, well_loc_5, well_loc_6, ...
+    well_loc_7, well_loc_8};
+
+
+for i = 1:num
+    x = floor(stats(i).Centroid(2));
+    y = floor(stats(i).Centroid(1));
+    for j = 1:8
+        % finds which cell centroid is in
+        im_holder = well_locs{1, j};
+        if im_holder(x, y) == 1
+            % j is well_loc
+            % finds the color of the well
+            gamestate.pixel_locs{j} = [x, y];
+            red = im.subtraction2(x, y, 1);
+            green = im.subtraction2(x, y, 2);
+            blue = im.subtraction2(x, y, 3);
+            % checks for which color the centoid is
+            % checks for red
+            if red == 255
+                gamestate.well_color(j) = 1;
+
+            elseif green == 255
+                gamestate.well_color(j) = 2;
+
+            elseif blue == 255
+                gamestate.well_color(j) = 3;
+
+            elseif red == 200 && green == 200
+                gamestate.well_color(j) = 4;
+
+            end
         end
     end
 end
+gamestate
 
-
-
-% gets a clean black and white image
-bw = im2bw(subtraction2, .8);
-bw = ~bw;
-SE = strel('disk', 2);
-erode = imerode(bw, SE);
-
-SE = strel('disk', 6);
-dialate = imdilate(erode, SE);
-
-%% gets the stats from the image
-stats = regionprops(dialate, 'Centroid');
-centroids = cat(1, stats.Centroid);
-imshow(bw);
-hold on
-plot(centroids(:,1),centroids(:,2),'bl*')
-hold off
-%% calculates the well locs
 
 
 
