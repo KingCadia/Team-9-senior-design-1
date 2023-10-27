6c0fd75183e850c1521d01f26461f5a3a0c7e416
KingCadia <57073780+KingCadia@users.noreply.github.com>
2023-10-27 09:13:31 -0400

    before computer dies

--- matlab_code/project_3/motor.m
+++ matlab_code/project_3/motor.m
@@ -1,19 +1,46 @@
-mega = arduino('COM3', 'Mega2560');
-time_period = 0.0000625;
-duty_cycle = 50;
-
-dir = 1;
-
-while 1
-    if dir == 1
-        writeDigitalPin(mega, 'D4', 1);
-        writeDigitalPin(mega, 'D5', 0);
-    else
-        writeDigitalPin(mega, 'D4', 0);
-        writeDigitalPin(mega, 'D5', 1);
+classdef motor
+    %MOTOR Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        Mega
+        IN1
+        IN2
+        Step_pin
+        pulses_per_degree
+        time_period
+        duty_cycle
     end
-    writeDigitalPin(mega, 'D2', 1);
-    pause(time_period * (duty_cycle / 100));
-    writeDigitalPin(mega, 'D2', 0);
-    pause(time_period * ((100 - duty_cycle) / 100));
+    
+    methods
+        function obj = motor(mega, IN1, IN2, Step_pin, time_period, duty_cycle)
+            % MOTOR Construct an instance of this class
+            % sets up the pins for the motors
+            obj.Mega = mega;
+            obj.IN1 = IN1;
+            obj.IN2 = IN2;
+            obj.Step_pin = Step_pin;
+            obj.pulses_per_degree = 5;
+            obj.time_period = time_period;
+            obj.duty_cycle = duty_cycle;
+            writeDigitalPin(obj.Mega, obj.Step_pin, 0);
+            writeDigitalPin(obj.Mega, obj.IN1, 1);
+            writeDigitalPin(obj.Mega, obj.IN2, 0);
+            
+        end
+        
+        % moves the motor the amount of degrees passed to the fuction the
+        % function is blocking my find a way to change that in the future
+        function move(obj, degrees)
+            stop_point = floor(degrees) * obj.pulses_per_degree;
+            i = 0;
+            while i < stop_point
+                writeDigitalPin(obj.Mega, obj.Step_pin, 1);
+                pause();
+            end
+        end
+        
+        
+    end
 end
+
